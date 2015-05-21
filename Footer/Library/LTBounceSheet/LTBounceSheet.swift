b139c88780d5b7519a1be6eae786669abc5d2079
pi <pi@pis-Mac-mini.local>
2015-05-21 19:07:47 +0300

    .

---
+++ Footer/Library/LTBounceSheet/LTBounceSheet.swift
@@ -0,0 +1,262 @@
+//
+//  LTBounceSheet.swift
+//  LTBounceSheet
+//
+//  Created by vale on 5/20/15.
+//  Copyright (c) 2015 changweitu@gmail.com. All rights reserved.
+//
+
+import UIKit
+let duration = Double(0.55)
+let sideViewDamping = CGFloat(0.87)
+let sideViewVelocity = CGFloat(10)
+let centerViewDamping = CGFloat(1.0)
+let centerViewVelocity = CGFloat(8)
+let footer = CGFloat(75)
+
+class LTBounceSheet: UIView {
+
+    private var sideHelperView: UIView!
+    private var centerHelperView: UIView!
+    private var displayLink: CADisplayLink!
+    private var contentView: UIView!
+    private var bgColor: UIColor!
+    
+    private var shown: Bool!
+    private var counter: Int!
+    private var height: CGFloat!
+    private let screenHeight: CGFloat!
+    
+    init(height: CGFloat, bgColor: UIColor) {
+        
+        self.height = height
+        self.bgColor = bgColor
+        self.counter = 0
+        self.shown = false
+        let screenRect = UIScreen.mainScreen().bounds
+        let screenWidth = CGRectGetWidth(screenRect)
+        screenHeight = CGRectGetHeight(screenRect)
+        
+        super.init(frame: CGRect(x: 0, y:screenHeight-height , width: screenWidth, height: height))
+        
+        
+        self.contentView = UIView(frame: CGRectMake(0, 0, screenWidth, screenHeight))
+        self.contentView.transform = CGAffineTransformMakeTranslation(0, height-footer)
+        self.contentView.backgroundColor = bgColor
+        self.addSubview(self.contentView)
+        
+        var swipeUp = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGesture:")
+        swipeUp.direction = UISwipeGestureRecognizerDirection.Up
+        self.addGestureRecognizer(swipeUp)
+        
+        var swipeDown = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGesture:")
+        swipeDown.direction = UISwipeGestureRecognizerDirection.Down
+        self.addGestureRecognizer(swipeDown)
+        
+        self.sideHelperView = UIView(frame: CGRectMake(0, height-footer, 0, 0))
+        self.sideHelperView.backgroundColor = UIColor.blackColor()
+        self.addSubview(self.sideHelperView)
+        
+        self.centerHelperView = UIView(frame: CGRectMake(screenWidth/2, height-footer, 0, 0))
+        self.addSubview(self.centerHelperView)
+        
+        self.backgroundColor = UIColor.clearColor()
+        UIApplication.sharedApplication().keyWindow?.addSubview(self)
+        
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func respondToSwipeGesture(gesture: UISwipeGestureRecognizer) {
+        
+        if gesture.direction == UISwipeGestureRecognizerDirection.Up {
+            
+        
+            self.show()
+            
+        } else {
+            
+         
+            self.hide()
+        }
+    }
+    func addView(view: UIView) {
+        
+        self.contentView.addSubview(view)
+    }
+    
+    func toggle() {
+        
+        if self.shown == true {
+            
+            self.hide()
+            
+        } else {
+            
+            self.show()
+            
+        }
+    }
+    
+    
+    func show() {
+        
+        if self.counter != 0 {
+            
+            return
+        }
+        self.shown = true
+        
+        self.start()
+        self.animateSideHelperViewToPoint(CGPointMake(self.sideHelperView.center.x, 0))
+        self.animateCenterHelperViewToPoint(CGPointMake(self.centerHelperView.center.x, 0))
+        self.animateContentViewToHeight(0)
+        
+    }
+    
+    func hide() {
+        
+        
+        if self.counter != 0 {
+            
+            return
+        }
+        self.shown = false
+        self.start()
+        let height = CGRectGetHeight(self.bounds)
+        self.animateSideHelperViewToPoint(CGPointMake(self.sideHelperView.center.x, height-footer))
+        self.animateCenterHelperViewToPoint(CGPointMake(self.centerHelperView.center.x, height-footer))
+        self.animateContentViewToHeight(self.height-footer)
+    }
+    
+    func animateSideHelperViewToPoint(point: CGPoint) {
+        
+        UIView.animateWithDuration(duration, delay: 0.0, usingSpringWithDamping: sideViewDamping, initialSpringVelocity: sideViewVelocity, options: UIViewAnimationOptions(0), animations: { () -> Void in
+            
+            self.sideHelperView.center = point
+            
+            }) { (finished: Bool) -> Void in
+                
+                self.complete()
+        }
+    
+    }
+    
+    func animateCenterHelperViewToPoint(point: CGPoint) {
+        
+        UIView.animateWithDuration(duration, delay: 0.0, usingSpringWithDamping: centerViewDamping, initialSpringVelocity: centerViewVelocity, options: UIViewAnimationOptions(0), animations: { () -> Void in
+            
+            self.centerHelperView.center = point
+            
+            }) { (finished: Bool) -> Void in
+                
+                self.complete()
+
+        }
+    }
+    func animateContentViewToHeight(height: CGFloat) {
+        
+        
+        UIView.animateWithDuration(duration, delay: 0.0, usingSpringWithDamping: centerViewDamping, initialSpringVelocity: centerViewVelocity, options: UIViewAnimationOptions(0), animations: ({
+            
+            self.contentView.transform = CGAffineTransformMakeTranslation(0, height);
+            
+        }), completion: nil)
+        
+    }
+    
+    func tick(displayLink: CADisplayLink) {
+        
+        self.setNeedsDisplay()
+    }
+    
+    func start() {
+        
+        if self.displayLink == nil {
+            
+            self.displayLink = CADisplayLink(target: self, selector: "tick:")
+            self.displayLink.addToRunLoop(NSRunLoop.currentRunLoop(), forMode: NSDefaultRunLoopMode)
+            
+            self.counter = 2
+        }
+    }
+    
+    func complete() {
+        
+        self.counter = self.counter-1
+        if self.counter==0 {
+            
+            self.displayLink.invalidate()
+            self.displayLink = nil
+          
+        }
+    }
+    
+    override func drawRect(rect: CGRect) {
+        
+        if self.counter == 0 {
+            
+            super.drawRect(rect)
+            return
+        }
+        
+        if self.sideHelperView.layer.presentationLayer() != nil {
+            
+            let sideLayer = self.sideHelperView.layer.presentationLayer() as! CALayer
+            let sidePoint = sideLayer.frame.origin
+            
+            let centerLayer = self.centerHelperView.layer.presentationLayer() as! CALayer
+            let centerPoint = centerLayer.frame.origin
+            
+            let path = UIBezierPath()
+            self.bgColor.setFill()
+            
+            path.moveToPoint(sidePoint)
+            path.addQuadCurveToPoint(CGPointMake(320, sidePoint.y), controlPoint: centerPoint)
+            path.addLineToPoint(CGPointMake(320, CGRectGetHeight(self.bounds)))
+            path.addLineToPoint(CGPointMake(0, CGRectGetHeight(self.bounds)))
+            path.closePath()
+            path.fill()
+        }
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+}
---
+++ Footer/ViewController.swift
@@ -0,0 +1,168 @@
+//
+//  ViewController.swift
+//  Footer
+//
+//  Created by Victor on 20/05/15.
+//
+//
+
+import UIKit
+
+let SHEET_COLOR = UIColor(red: 255/255.0, green: 255/255.0, blue: 255/255.0, alpha: 1)
+
+let DUMMYIMG_URL = "http://dummyimage.com/100x100/000000/fff.png&text="
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+
+    var sheet: LTBounceSheet!
+    var shown: Bool = false
+    var badgeCount:Int = 10
+    var currentSelectedBadgeInd:Int = -1
+
+    @IBOutlet var mFooterView: UIView!
+    @IBOutlet var mProfileImageView:UIImageView!
+    @IBOutlet var mProfileRatingView :JWStarRatingView!
+    @IBOutlet var m2ndRowScrView :UIScrollView!
+    @IBOutlet var m3rdRowScrView :UIScrollView!
+    @IBOutlet var mPageControl :UIPageControl!
+    @IBOutlet var mToggleButton :UIButton!
+    
+    //initialize 2nd scrollview
+    func setUp2ndScrollView()
+    {
+        var centerX : CGFloat = 30
+        var ind:Int = 0
+        
+        for (ind = 0; ind < badgeCount; ind++)
+        {
+            var imageView = UIImageView(frame: CGRectMake(0, 0, 40, 40))
+            imageView.tag = ind
+            imageView.setImageWithURL(NSURL(string:DUMMYIMG_URL + String(ind + 1) as String))
+            imageView.center = CGPointMake(centerX, 30)
+            imageView.layer.cornerRadius = imageView.frame.size.width / 2
+            imageView.layer.masksToBounds = true
+            imageView.userInteractionEnabled = true
+            
+            var tapGes = UITapGestureRecognizer(target: self, action: "respondToTapGesture:")
+            tapGes.numberOfTapsRequired = 1
+            imageView.addGestureRecognizer(tapGes)
+            
+            m2ndRowScrView.addSubview(imageView)
+            
+            centerX = centerX + 60;
+        }
+        m2ndRowScrView.contentSize = CGSizeMake(centerX - 30, m2ndRowScrView.frame.size.height)
+    }
+    
+    //initialize 3rd scrollview
+    func setUp3rdScrollView()
+    {
+        var centerX : CGFloat = m3rdRowScrView.frame.size.width / 2
+        var ind:Int = 0
+        for (ind = 0; ind < 5; ind++)
+        {
+            //            var ratingView = JWStarRatingView()
+            //            ratingView.frame = CGRectMake(0, 0, 120, 40)
+            //            ratingView.center = CGPointMake(centerX, 30)
+            //            m3rdRowScrView.addSubview(ratingView)
+            
+            var label = UILabel(frame: CGRectMake(0, 0, m3rdRowScrView.frame.size.width, m3rdRowScrView.frame.size.height))
+            label.center = CGPointMake(centerX, m3rdRowScrView.frame.size.height / 2)
+            label.text = "This is Comment" + String(ind + 1)
+            label.textAlignment = NSTextAlignment.Center
+            m3rdRowScrView.addSubview(label)
+            
+            centerX = centerX + self.view.frame.size.width;
+        }
+        m3rdRowScrView.contentSize = CGSizeMake(centerX - self.view.frame.size.width / 2, m3rdRowScrView.frame.size.height)
+        m3rdRowScrView.delegate = self
+        mPageControl.numberOfPages = ind
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        super.viewDidAppear(animated)
+        self.sheet = LTBounceSheet(height: 250, bgColor: SHEET_COLOR)
+        mFooterView.frame = CGRectMake(0, 0, mFooterView.frame.size.width, mFooterView.frame.size.height);
+        self.sheet.addView(mFooterView)
+        
+        mProfileImageView.setImageWithURL(NSURL(string: DUMMYIMG_URL + "P"))
+        mProfileImageView.layer.cornerRadius = mProfileImageView.frame.size.width/2
+        mProfileImageView.layer.masksToBounds = true;
+
+        setUp2ndScrollView()
+        setUp3rdScrollView()
+    }
+    
+    @IBAction func onPageControl(sender: UIPageControl)
+    {
+        var selectedInd:CGFloat = CGFloat(sender.currentPage)
+        m3rdRowScrView.contentOffset = CGPointMake(selectedInd * m3rdRowScrView.frame.size.width, 0)
+    }
+    
+    @IBAction func onShowSheet(sender:UIButton)
+    {
+        if shown == true
+        {
+            sheet.hide();
+            mToggleButton.setTitle("+", forState: UIControlState.Normal)
+            shown = false
+        }
+        else
+        {
+            sheet.show();
+            mToggleButton.setTitle("-", forState: UIControlState.Normal)
+            shown = true
+        }
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView)
+    {
+        var selInd:Int = Int(scrollView.contentOffset.x / scrollView.frame.size.width)
+        mPageControl.currentPage = selInd;
+    }
+    
+    //expand badge with label when badge is tapped
+    func respondToTapGesture(gesture: UISwipeGestureRecognizer)
+    {
+        var imageView:UIImageView = gesture.view as! UIImageView
+        var selectedInd:Int = imageView.tag
+        
+        var centerX : CGFloat = 30
+        var ind:Int = 0
+        if currentSelectedBadgeInd == selectedInd
+        {
+            for (ind = 0; ind < badgeCount; ind++)
+            {
+                var subView:UIImageView = Array(m2ndRowScrView.subviews)[ind] as! UIImageView
+                subView.center = CGPointMake(centerX, 30)
+                centerX = centerX + 60
+            }
+            
+            currentSelectedBadgeInd = -1
+        }
+        else
+        {
+            for (ind = 0; ind < badgeCount; ind++)
+            {
+                var subView:UIImageView = Array(m2ndRowScrView.subviews)[ind] as! UIImageView
+                subView.center = CGPointMake(centerX, 30)
+                centerX = centerX + 60
+                
+                if ind == selectedInd
+                {
+                    centerX = centerX + 120
+                }
+            }
+            
+            currentSelectedBadgeInd = selectedInd
+        }
+        
+        m2ndRowScrView.contentSize = CGSizeMake(centerX - 30, m2ndRowScrView.frame.size.height)
+
+    }
+}
